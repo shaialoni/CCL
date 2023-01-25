8ed6b9c8b06cf2ede899c47a4ec1b87d67c63dd1
shaialoni <alonishai@gmail.com>
2023-01-25 09:57:06 -0800

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Contra Costa Locksmith`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-theme-ui", "gatsby-plugin-google-gtag"]
+};
